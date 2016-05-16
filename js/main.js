b95e09c39eac8c2e93b36071bcf4a0e3e5eb701c
emilyvivancocuba <emilyvivancocuba@gmail.com>
2016-05-16 15:55:38 -0500

    avanzado 90%

--- js/main.js
+++ js/main.js
@@ -3,11 +3,18 @@ function aleatorio(min,max){
    	var num_aleatorio=parseInt(min) + Math.round(Math.random() * (max-min));
    	return num_aleatorio;
 }
-var sede_seleccionada = document.getElementById('sede_seleccionada');
-sede_seleccionada.onchange=function(){
+function aparecer_imagen(){
+	img=document.createElement('img');
+	alea=aleatorio(0, (pais.length)-1);
+	foto_coder=pais[alea].image;
+	name_coder=pais[alea].name;
+    img.setAttribute('src', 'fotos/'+name_pais+'/'+foto_coder);
+    img.setAttribute('alt', name_coder);
+    img.setAttribute('class', 'foto_chicacoder');
+    document.getElementById('foto-box').appendChild(img);
+}
+function seleccionar_pais(){
 	var sede_valor=sede_seleccionada.options[sede_seleccionada.selectedIndex].textContent;
-	var pais;
-	var name_pais;
 	if (sede_valor==='Perú-Lima') {
 		pais=peru;
 		name_pais='peru'
@@ -15,76 +22,44 @@ sede_seleccionada.onchange=function(){
 		pais=mexico;
 		name_pais='mexico';
 	}
-	var img=document.createElement('img');
-	var alea=aleatorio(0, (pais.length)-1);
-	var foto_coder=pais[alea].image;
-	var name_coder=pais[alea].name;
+	img=document.createElement('img');
+	alea=aleatorio(0, (pais.length)-1);
+	foto_coder=pais[alea].image;
+	name_coder=pais[alea].name;
     img.setAttribute('src', 'fotos/'+name_pais+'/'+foto_coder);
+    img.setAttribute('alt', name_coder);
     img.setAttribute('class', 'foto_chicacoder');
     document.getElementById('foto-box').appendChild(img);
-    return name_coder;
 }
-var nombre_coder_ingresado=document.getElementById('name_coder_ingresado').value;
-if (nombre_coder_ingresado===name_coder) {
-	alert("Es correcto");
+function comprobacion(){
+	respuesta=document.getElementById('respuesta');
+	var nombre_coder_ingresado=document.getElementById('name_coder_ingresado').value;
+	if (nombre_coder_ingresado.toLowerCase()===name_coder.toLowerCase()) {
+		puntaje+=5;
+		respuesta.innerHTML='Bien hecho, ganaste 5 puntos.\nAhora tienes '+puntaje+' puntos';
+		$(".foto_chicacoder").remove();
+		aparecer_imagen();
+	}else{
+		if(nombre_coder_ingresado.toLowerCase()!==name_coder.toLowerCase()){
+			puntaje-=1;
+			respuesta.innerHTML='No es correcto. Perdiste un punto\nTe quedan '+(4-i)+' intentos\nAhora tienes '+puntaje+' puntos';
+			i++;
+			if (i===5) {
+				i=0;
+				$(".foto_chicacoder").remove();
+				aparecer_imagen();
+			}
+		}else{
+			$(".foto_chicacoder").remove();
+			aparecer_imagen();
+		}
+	}
 }
+var name_coder, name_pais, pais, alea, respuesta, puntaje, i, foto_coder, img, peru, mexico;
+puntaje=0;
+i=0;
+var sede_seleccionada = document.getElementById('sede_seleccionada');
+sede_seleccionada.addEventListener('change', seleccionar_pais);
+var boton=document.getElementById('boton');
+boton.addEventListener('click', comprobacion);
 
-
-
-
-
-
-
-
-
-
-
-
-// resetear.onclick=reset();
-// var contar=0;
-// var suma=0;
-// for (var i=0; i<9; i++) {
-// 	boton_jugar[i].onclick=function(){
-// 		contar++;
-// 		suma++;
-// 		if (contar%2===0) {
-// 			this.textContent = 'o';
-// 		}else{
-// 			this.textContent = 'x';
-// 		}
-// 		this.setAttribute('disabled', false);
-// 		if (suma>=5) {
-// 			if (($('#1').text()==$('#2').text()&&$('#1').text()==$('#3').text())||($('#1').text()==$('#4').text()&&$('#1').text()==$('#7').text())||($('#1').text()==$('#5').text()&&$('#1').text()==$('#9').text())) {
-// 				if ($('#1').text()=='x') {
-// 					alert('El ganador es x');
-// 					reset();
-// 				}
-// 				if ($('#1').text()=='o') {
-// 					alert('El ganador es o');
-// 					reset();
-// 				}
-// 			}else if (($('#2').text()==$('#5').text()&&$('#5').text()==$('#8').text())||($('#3').text()==$('#5').text()&&$('#5').text()==$('#7').text())||($('#4').text()==$('#5').text()&&$('#5').text()==$('#6').text())) {
-// 				if ($('#5').text()=='x') {
-// 					alert('El ganador es x');
-// 					reset();
-// 				}
-// 				if ($('#5').text()=='o') {
-// 					alert('El ganador es o');
-// 					reset();
-// 				}
-// 			}else if (($('#3').text()==$('#6').text()&&$('#6').text()==$('#9').text())||($('#7').text()==$('#8').text()&&$('#8').text()==$('#9').text())) {
-// 				if ($('#9').text()=='x') {
-// 					alert('El ganador es x');
-// 					reset();
-// 				}
-// 				if ($('#9').text()=='o') {
-// 					alert('El ganador es o');
-// 					reset();
-// 				}
-// 			}else if (suma===9) {
-// 				alert('JUEGO EMPATADO');
-// 				reset();
-// 			}
-// 		}
-// 	}
-// }
